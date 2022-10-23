6824a387b92bbdeb85e12fe911bdd931ab645894
nidhi <inidhi99@gmail.com>
2022-10-23 19:13:44 -0400

    added today's and 5 day weather along with API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var searchHistory = [];
 var apiUrl = "https://api.openweathermap.org";
-// var apiKey = ??
+var apiKey = "0a4ec138381a2dd46c384597d3f1044f";
 
 var searchForm = document.querySelector("#search-form");
+var searchBtn = document.getElementById ("search-button");
 var searchInput = document.querySelector("#search-input");
 var todayContainer = document.querySelector("#today");
 var forecastContainer = document.querySelector("#forecast");
@@ -50,8 +51,67 @@ function getStorage (){
 }
 
 //to do: function render current weather 
+function currentWeather (e){
+    e.preventDefault()
+    let cityName = document.getElementById("cityName").value;
+    let url= `${apiUrl}/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+    fetch(url)
+    .then ((repsonse) => repsonse.json())
+    .then((data) => {
+        console.log(data)
+        let today_div = document.getElementById("today")
+
+        let temp_para = document.createElement("p")
+        temp_para.innerHTML = `Temp is: ${data.main.temp}.`
+
+        let wind_para = document.createElement("p")
+        wind_para.innerHTML = `wind is: ${data.wind.speed}.`
+        today_div.append(temp_para, wind_para);
+        
+
+
+        forecastWeather(cityName);
+    });
+} 
+
+searchBtn.addEventListener("click", currentWeather );
+
+
 // to do: function render 5 day forecast card 
+function forecastWeather (cityName){
+    let forecastUrl = `${apiUrl}/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+    fetch(forecastUrl)
+    .then ((repsonse) => repsonse.json())
+    .then((data) => {
+        console.log(data)
+        let forecastList = data.list
+        let parentDiv = document.createElement("div")
+        parentDiv.style.display = "flex"
+
+
+
+        for (let i= 0; i< forecastList.length; i+=8){
+            let forecastDiv = document.createElement("div")
+            forecastDiv.style.border = "1px solid black"
+            
+        let temp_para = document.createElement("p")
+        temp_para.innerHTML = `Temp is: ${forecastList[i].main.temp}.`
+
+        let wind_para = document.createElement("p")
+        wind_para.innerHTML = `wind is: ${forecastList[i].wind.speed}.`
+        forecastDiv.append(temp_para, wind_para);
+        parentDiv.append(forecastDiv)
+   
+        }
+document.getElementById("forecast").append(parentDiv)
+
+    } );
+
+}
+
 // to do: function *5 to get 5 cards (start date - end date )
+
+
 // to do: function api calls - 2 separte api call - city name (lat and long ) - api for the data 
 // to do: function to handle the submit of the search form 
 // to do: function to handle the click
